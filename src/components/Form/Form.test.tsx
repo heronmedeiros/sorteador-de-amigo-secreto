21f447d7a851d1f0eabb8d4805933a7ed1cdc1bb
Heron Medeiros <heron.medeiros@gmail.com>
2023-10-14 11:00:05 -0300

    Avoid duplicated names

--- src/components/Form/Form.test.tsx
+++ src/components/Form/Form.test.tsx
@@ -1,9 +1,14 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import Form from "./Form";
+import { RecoilRoot } from "recoil";
 
 //Jest
 test('Quando o input esta vazio, novos participantes não podem ser adicionados', () => {
-  render(<Form />);
+  render(
+    <RecoilRoot>
+      <Form />
+    </RecoilRoot>
+  );
   //encontrar no DOM o input
   const input = screen.getByPlaceholderText('Insira os nomes dos participantes');
   //encontrar o botão
@@ -13,3 +18,65 @@ test('Quando o input esta vazio, novos participantes não podem ser adicionados'
   //garantir que o botão esteja desabilitado
   expect(button).toBeDisabled();
 });
+
+test('adicionar um participante caso exista um nome preenchido', () => {
+  render(
+    <RecoilRoot>
+      <Form />
+    </RecoilRoot>
+  );
+   //encontrar no DOM o input
+   const input = screen.getByPlaceholderText('Insira os nomes dos participantes');
+   //encontrar o botão
+   const button = screen.getByRole('button');
+
+  // inserir um valor no input
+  fireEvent.change(input, {
+    target: {
+      value: 'Ana Catarina'
+    }
+  })
+  //clicar no botão de submeter
+  fireEvent.click(button);
+  //garantir que o input esteja com o foco ativo
+  expect(input).toHaveFocus();
+  //garantir que o input não tenha um valor
+  expect(input).toHaveValue("");
+})
+
+
+test('Nomes duplicados não podem ser adicionados na lista ', () => {
+  render(
+    <RecoilRoot>
+      <Form />
+    </RecoilRoot>
+  );
+   //encontrar no DOM o input
+   const input = screen.getByPlaceholderText('Insira os nomes dos participantes');
+   //encontrar o botão
+   const button = screen.getByRole('button');
+
+  // inserir um valor no input
+  fireEvent.change(input, {
+    target: {
+      value: 'Ana Catarina'
+    }
+  })
+
+  //clicar no botão de submeter
+  fireEvent.click(button);
+
+    // inserir um valor no input
+    fireEvent.change(input, {
+      target: {
+        value: 'Ana Catarina'
+      }
+    })
+
+    //clicar no botão de submeter
+    fireEvent.click(button);
+
+  const mensagemDeErro = screen.getByRole('alert');
+
+  expect(mensagemDeErro.textContent).toBe('Nomes duplicados não são permitidos');
+})
--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,8 +1,33 @@
+import { useRef, useState } from "react";
+import { useAdicionarParticipante } from "../../state/hook/useAdicionarParticipante";
+import { useMensagemDeErro } from "../../state/hook/useMensagemDeErro";
+
 const Form = () => {
+  const [nome, setNome] = useState('');
+
+  const inputRef = useRef<HTMLInputElement>(null);
+  const adicionarNaLista = useAdicionarParticipante();
+
+  const mensagemDeErro = useMensagemDeErro();
+
+  const adicionarPartipante = (evento : React.FormEvent<HTMLFormElement>) => {
+    evento.preventDefault();
+    adicionarNaLista(nome);
+    setNome('');
+    inputRef.current?.focus();
+  }
+
   return (
-    <form>
-      <input type="text" placeholder="Insira os nomes dos participantes" />
-      <button disabled={true}>Adicionar</button>
+    <form onSubmit={adicionarPartipante}>
+      <input
+        type="text"
+        placeholder="Insira os nomes dos participantes"
+        value={nome}
+        onChange={evento => setNome(evento.target.value)}
+        ref={inputRef}
+      />
+      <button disabled={!nome}>Adicionar</button>
+      { mensagemDeErro && <p role="alert">{mensagemDeErro}</p> }
     </form>
   )
 }
