a8e970d55706d58f1dab55d7c232e8a07d79ec87
Heron Medeiros <heron.medeiros@gmail.com>
2023-10-14 10:11:27 -0300

    Added first test

---
+++ src/components/Form/Form.test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from "@testing-library/react";
+import Form from "./Form";
+
+//Jest
+test('Quando o input esta vazio, novos participantes não podem ser adicionados', () => {
+  render(<Form />);
+  //encontrar no DOM o input
+  const input = screen.getByPlaceholderText('Insira os nomes dos participantes');
+  //encontrar o botão
+  const button = screen.getByRole('button');
+  //garantir que o input esteja no documento
+  expect(input).toBeInTheDocument();
+  //garantir que o botão esteja desabilitado
+  expect(button).toBeDisabled();
+});
